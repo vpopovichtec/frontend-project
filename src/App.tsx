1a13fa400a80e1774cf7d6e90dfe564d27e1207a
Vladimir Popovic <vladimir.popovic@htecgroup.com>
2026-07-01 17:46:58 +0200

    Add fetching of popular movies and console output

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,38 @@
 import './App.css'
+import { useEffect } from 'react'
+
+const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+const BASE_URL = "https://api.themoviedb.org/3";
+
 
 function App() {
+  useEffect(() => {
+    const fetchPopularMovies = async () => {
+      try {
+        const options = {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${TMDB_TOKEN}`,
+            accept: "application/json"
+          }
+        }
+
+        const response = await fetch(`${BASE_URL}/movie/popular`, options);
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchPopularMovies();
+  }, [])
+
   return (
     <div>
         Hello
