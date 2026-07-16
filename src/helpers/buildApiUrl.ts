e9246f639ff2dfbb8483a664089f341be0a465fb
Vladimir Popovic <vladimir.popovic@htecgroup.com>
2026-07-16 12:11:29 +0200

    Add return type to buildApiUrl

--- src/helpers/buildApiUrl.ts
+++ src/helpers/buildApiUrl.ts
@@ -1,6 +1,6 @@
 const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
 
-export const buildApiUrl = (...pathSegments: String[]) => {
+export const buildApiUrl = (...pathSegments: String[]): String => {
   /*
   Join BASE_URL + endpoint (or multiple endpoints)
   clean-up if there are duplicate '//' if we are joining the enpoint from the contant
