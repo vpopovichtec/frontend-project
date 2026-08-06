6e1c0afd2cb7cfd7551504f5fc7d04fd23031821
Vladimir Popovic <vladimir.popovic@htecgroup.com>
2026-08-06 13:33:35 +0200

    Add Storybook and MovieCard stories

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,11 +1,12 @@
-import type { Preview } from '@storybook/react-vite'
+import type { Preview } from "@storybook/react-vite";
+import "../src/index.css";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
@@ -13,9 +14,15 @@ const preview: Preview = {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: 'todo'
-    }
+      test: "todo",
+    },
   },
+  decorators: [
+    (Story) => {
+      document.documentElement.classList.add("dark");
+      return <Story />;
+    },
+  ],
 };
 
 export default preview;
---
+++ src/components/MovieCard.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { MovieCard } from "./MovieCard";
+import { mockResponse } from "@/test/mockResponse";
+
+const movie = mockResponse.results[0];
+
+const meta = {
+  // 👇 The component you're working on
+  title: "Components/MovieCard",
+  component: MovieCard,
+  decorators: [
+    (StoryComponent) => (
+      <div className="p-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
+        <StoryComponent />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof MovieCard>;
+
+export default meta;
+// 👇 Type helper to reduce boilerplate
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: movie,
+};
+
+export const LongTitle: Story = {
+  args: {
+    ...movie,
+    original_title:
+      "Batman Returns With An Extremely Long Movie Title That Should Wrap Nicely",
+  },
+};
